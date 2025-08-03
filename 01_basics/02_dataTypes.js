37b6e55a15a959f96f8a7c8ce40259bfebf6b99e
Sarthak Gautam <gautamsarthak72@gmail.com>
2025-08-03 10:31:54 +0000

    dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//tereat all js code as newer version 
+
+//alert('Hello world') we are using node js not broweser 
+
+console.log ("Hello world")
+console.log(3+3)//code rediability should good
+
+let age=18
+let isLoggedIn=false
+let state;
+//number=>2 to power 53
+//bigint
+//string=>"
+// boolean=>true/false
+// undefined
+// null=>empty"
+//symbol
+
+//object
+
+console.log(typeof "undefined ")
+ console.log(typeof "null")
